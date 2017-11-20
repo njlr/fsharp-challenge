26d19a4957b3927e19c92d97a884516275920579
njlr <njlr@users.noreply.github.com>
2017-11-20 17:27:19 +0000

    * Migrated to Paket + Fake setup  * Added Suave dependency

--- README.md
+++ README.md
@@ -2,8 +2,25 @@
 
 ## Build Instructions
 
-The code has been tested on an Ubuntu machine with F# 4.0
+Fetch dependencies: 
 
 ```bash=
-./build.sh && ./Program.exe
+mono .paket/paket.bootstrapper.exe
+mono .paket/paket.exe install
 ```
+
+Build: 
+
+```bash=
+./build.sh
+```
+
+Run: 
+
+```bash=
+dotnet ./Conway/bin/Debug/netcoreapp2.0/Conway.dll 
+```
+
+## Dev Tools
+
+The recommended tooling is [Visual Studio Code](https://code.visualstudio.com/) with the [Ionide](http://ionide.io/) extenstion. 
--- build.sh
+++ build.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-fsharpc CellularAutomata.fs CLI.fs Program.fs --out:Program.exe && chmod +x Program.exe
+if test "$OS" = "Windows_NT"
+then
+  # use .Net
+
+  .paket/paket.exe restore
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+  	exit $exit_code
+  fi
+
+  packages/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx
+else
+  # use mono
+  mono .paket/paket.exe restore
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+  	exit $exit_code
+  fi
+  mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
+fi
